fe4fc2b7f5d20eb30b7072875e8a525391631c82
Just your love <76810719+itsmeNaraa@users.noreply.github.com>
2023-07-18 09:33:25 +0800

    moonstone: Init BlissROMs

--- bliss_moonstone.mk
+++ bliss_moonstone.mk
@@ -9,15 +9,20 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/bliss/config/common_full_phone.mk)
 
 # Inherit from moonstone device
 $(call inherit-product, device/xiaomi/moonstone/device.mk)
 
-PRODUCT_NAME := lineage_moonstone
+PRODUCT_NAME := bliss_moonstone
 PRODUCT_DEVICE := moonstone
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := POCO X5 5G
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="moonstone-user 13 TKQ1.221114.001 V14.0.3.0.TMPMIXM release-keys"
+
+BUILD_FINGERPRINT := POCO/moonstone_p_global/moonstone:13/TKQ1.221114.001/V14.0.3.0.TMPMIXM:user/release-keys
