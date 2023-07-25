a9135ea4916b90e11c7ecf6f9ef30eb06566683b
Just your love <76810719+itsmeNaraa@users.noreply.github.com>
2023-07-25 17:03:15 +0700

    Fix error device tree in ArrowOS branch

--- arrow_moonstone.mk
+++ arrow_moonstone.mk
@@ -7,9 +7,18 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/arrow/config/common_full_phone.mk)
+# Inherit some common arrow stuff.
+$(call inherit-product, vendor/arrow/config/common.mk)
+
+# ArrowOS additions
+DEVICE_MAINTAINER := Kristoforusapm
+
+# Inherit some common device props
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_SUPPORTS_CALL_RECORDING := true
 
 # Inherit from moonstone device
 $(call inherit-product, device/xiaomi/moonstone/device.mk)
@@ -19,6 +28,9 @@ PRODUCT_DEVICE := moonstone
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := POCO X5 5G
+
+# Boot animation
+TARGET_BOOT_ANIMATION_RES := 1080
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
